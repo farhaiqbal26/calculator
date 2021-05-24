eaa426a974c61e3824f8a991a91ecf44ca39a045
abhirami <amicheeku007@gmail.com>
2021-05-24 11:22:33 +0530

    modified

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print("sample")
+
+def subtract(x, y):
+    return x - y
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print(num1, "-", num2, "=", subtract(num1, num2))
