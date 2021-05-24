d96534580517942fd91af5834fd2d1594a443acd
fawazpoleri <48981506+fawazpoleri@users.noreply.github.com>
2021-05-24 12:13:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
 print("sample")
 
-def subtract(x, y):
-    return x - y
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-print(num1, "-", num2, "=", subtract(num1, num2))
